cae91c5ea4da64cfee2bed8efb816d0915472f77
EduardoJ20 <151193647+EduardoJ20@users.noreply.github.com>
2023-11-30 17:40:32 -0600

    Ola
    Ola

---
+++ AgenciaF/src/main/java/com/mycompany/agenciaf/AgenciaF.java
@@ -0,0 +1,11 @@
+package com.mycompany.agenciaf;
+
+import Interfaz.InicioSesion;
+
+public class AgenciaF {
+     public static void main(String[] args) 
+    {
+        InicioSesion Is=new InicioSesion();
+        Is.setVisible(true);
+    }
+}
